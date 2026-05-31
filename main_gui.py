02ca55953b5b2a7a963968e77d40caac49746690
la_n <la_n@foxmail.com>
2026-05-31 11:44:22 +0800

    add dist/LogMonitoringTool.exe

--- main_gui.py
+++ main_gui.py
@@ -1,10 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from gui.main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("1f310.ico"))
     app.setStyle("Fusion")
     window = MainWindow()
     window.show()
